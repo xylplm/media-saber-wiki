2ee003815204bc26f78fd71a1f239ca97f4e0f42
xylplm <luoxiao123@outlook.com>
2023-06-30 10:35:54 +0800

    init project

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+
+push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+commit_info=`git describe --all --always --long`
+dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
+push_branch=gh-pages # 推送的分支
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd $dist_path
+
+git init
+git add -A
+git commit -m "deploy, $commit_info"
+git push -f $push_addr HEAD:$push_branch
+
+cd -
+rm -rf $dist_path
