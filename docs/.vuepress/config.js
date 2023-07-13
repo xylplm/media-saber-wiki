c287bcb82bb79b2b8f8dcb8c0f6031bd82960de1
luoxiao <luoxiao123@outlook.com>
2023-07-13 21:14:33 +0800

    修复评论

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,45 +1,45 @@
-const htmlModules = require('./config/htmlModules.js');
-
+const htmlModules = require("./config/htmlModules.js");
 
 module.exports = {
-
-  theme: 'vdoing', // 使用依赖包主题
+  theme: "vdoing", // 使用依赖包主题
   // theme: require.resolve('../../vdoing'), // 使用本地主题 (先将vdoing主题文件下载到本地：https://github.com/xugaoyi/vuepress-theme-vdoing)
 
   title: "Media Saber Wiki",
-  description: 'Media Saber Wiki 文档',
+  description: "Media Saber Wiki 文档",
   // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
-  head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-    ['link', { rel: 'icon', href: '/img/logo.png' }], //favicons，资源放在public文件夹
-    ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
-    ['meta', { name: 'theme-color', content: '#5e72e4' }], // 移动浏览器主题颜色
-    ['script', { charset: 'UTF-8', id: 'LA_COLLECT', src: '//sdk.51.la/js-sdk-pro.min.js' }],
-    ['script', {}, `LA.init({id:"K4jpll24hC6vwBJ0",ck:"K4jpll24hC6vwBJ0"})`]
+  head: [
+    // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
+    ["link", { rel: "icon", href: "/img/logo.png" }], //favicons，资源放在public文件夹
+    ["meta", { name: "keywords", content: "vuepress,theme,blog,vdoing" }],
+    ["meta", { name: "theme-color", content: "#5e72e4" }], // 移动浏览器主题颜色
+    ["script", { charset: "UTF-8", id: "LA_COLLECT", src: "//sdk.51.la/js-sdk-pro.min.js" }],
+    ["script", {}, `LA.init({id:"K4jpll24hC6vwBJ0",ck:"K4jpll24hC6vwBJ0"})`],
   ],
 
   // 主题配置
   themeConfig: {
     nav: [
-      { text: '主页', link: '/' },
-      { text: '文档', link: '/docs/start/info/' },
-      { text: '反馈', link: 'https://github.com/xylplm/media-saber-wiki/issues' },
-      { text: 'Docker', link: 'https://hub.docker.com/r/xylplm/media-saber' },
+      { text: "主页", link: "/" },
+      { text: "文档", link: "/docs/start/info/" },
+      { text: "反馈", link: "https://github.com/xylplm/media-saber-wiki/issues" },
+      { text: "Docker", link: "https://hub.docker.com/r/xylplm/media-saber" },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/logo.png', // 导航栏logo
+    logo: "/img/logo.png", // 导航栏logo
     //repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
-    lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+    lastUpdated: "上次更新", // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
     // docsDir: 'docs', // 编辑的文件夹
     // editLinks: true, // 编辑链接
     // editLinkText: '编辑',
 
     // 以下配置是Vdoing主题改动的和新增的配置
-    sidebar: { mode: 'structuring', collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+    sidebar: { mode: "structuring", collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
 
     // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
-    updateBar: { // 最近更新栏
+    updateBar: {
+      // 最近更新栏
       showToArticle: false, // 显示到文章页底部，默认true
       // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
     },
@@ -49,7 +49,7 @@ module.exports = {
     //   '图标地址2'
     // ],
 
-    pageStyle: 'line', // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
+    pageStyle: "line", // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
 
     // contentBgStyle: 1,
 
@@ -57,13 +57,15 @@ module.exports = {
     tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
     // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
 
-    author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
-      name: 'xylplm', // 必需
-      href: '' // 可选的
+    author: {
+      // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
+      name: "xylplm", // 必需
+      href: "", // 可选的
     },
-    footer: { // 页脚信息
+    footer: {
+      // 页脚信息
       createYear: 2023, // 博客创建年份
-      copyrightInfo: 'msaber.fun | MIT License', // 博客版权信息，支持a标签
+      copyrightInfo: "msaber.fun | MIT License", // 博客版权信息，支持a标签
     },
     htmlModules,
   },
@@ -75,7 +77,7 @@ module.exports = {
     //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
     // }],
 
-    ['fulltext-search'], // 全文搜索
+    ["fulltext-search"], // 全文搜索
 
     // ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
     //   thirdparty: [ // 可选，默认 []
@@ -94,13 +96,17 @@ module.exports = {
     //     }
     //   ]
     // }],
-    
-    ['one-click-copy', { // 代码块复制按钮
-      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
-      duration: 1000, // prompt message display time.
-      showInMobile: false // whether to display on the mobile side, default: false.
-    }],
+
+    [
+      "one-click-copy",
+      {
+        // 代码块复制按钮
+        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+        copyMessage: "复制成功", // default is 'Copy successfully and then paste it for use.'
+        duration: 1000, // prompt message display time.
+        showInMobile: false, // whether to display on the mobile side, default: false.
+      },
+    ],
     // ['demo-block', { // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
     //   settings: {
     //     // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
@@ -112,46 +118,43 @@ module.exports = {
     //   }
     // }],
     [
-      'vuepress-plugin-zooming', // 放大图片
+      "vuepress-plugin-zooming", // 放大图片
       {
-        selector: '.theme-vdoing-content img:not(.no-zoom)',
+        selector: ".theme-vdoing-content img:not(.no-zoom)",
         options: {
-          bgColor: 'rgba(0,0,0,0.6)'
+          bgColor: "rgba(0,0,0,0.6)",
         },
       },
     ],
     [
-      '@vuepress/last-updated', // "上次更新"时间格式
+      "@vuepress/last-updated", // "上次更新"时间格式
       {
         transformer: (timestamp, lang) => {
-          const dayjs = require('dayjs') // https://day.js.org/
-          return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+          const dayjs = require("dayjs"); // https://day.js.org/
+          return dayjs(timestamp).format("YYYY/MM/DD, HH:mm:ss");
         },
-      }
+      },
     ],
     [
-      'vuepress-plugin-vdoing-comment',
+      "vuepress-plugin-vdoing-comment",
       {
-          choosen: 'artalk',
-          options: {
-              server: 'https://artk.musnow.top', // （必填）
-              site: 'khl-py', // （必填）
-              // disableEmotion: false, // 是否禁用表情（可选）
-              // disablePicture: true, // 是否禁用图片（可选）
-              // disablePreview: false // 是否禁用预览（可选）
-          }
-      }
-  ]
+        choosen: "artalk",
+        options: {
+          server: "https://wiki.msaber.fun/", // （必填）
+          site: "Media Saber", // （必填）
+          // disableEmotion: false, // 是否禁用表情（可选）
+          // disablePicture: true, // 是否禁用图片（可选）
+          // disablePreview: false // 是否禁用预览（可选）
+        },
+      },
+    ],
   ],
 
   markdown: {
     // lineNumbers: true,
-    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+    extractHeaders: ["h2", "h3", "h4", "h5", "h6"], // 提取标题到侧边栏的级别，默认['h2', 'h3']
   },
 
   // 监听文件变化并重新构建
-  extraWatchFiles: [
-    '.vuepress/config.js',
-    '.vuepress/config/htmlModules.js',
-  ]
-}
+  extraWatchFiles: [".vuepress/config.js", ".vuepress/config/htmlModules.js"],
+};
