de2ed9a4196095005def098c45b13ca1c1afcec8
luoxiao <luoxiao123@outlook.com>
2023-08-05 15:27:13 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,7 +27,7 @@ module.exports = {
       { text: "反馈", link: "https://github.com/xylplm/media-saber-wiki/issues" },
       { text: "Docker", link: "https://hub.docker.com/r/xylplm/media-saber" },
     ],
-    sidebarDepth: 1, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: "/img/logo.png", // 导航栏logo
     repo: "xylplm/media-saber-wiki", // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
