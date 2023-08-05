8a16fa1ae7094130632fe2c61634bc47ed2bf306
luoxiao <luoxiao123@outlook.com>
2023-08-05 18:08:56 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,8 +34,9 @@ module.exports = {
     lastUpdated: "上次更新", // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
     docsDir: "docs", // 编辑的文件夹
+    docsBranch: "main",
     editLinks: true, // 编辑链接
-    editLinkText: "编辑",
+    editLinkText: "帮助我们改善此页面！",
 
     // 以下配置是Vdoing主题改动的和新增的配置
     sidebar: { mode: "structuring", collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
