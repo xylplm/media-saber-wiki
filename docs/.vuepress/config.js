d7c322903c2a44a32e58453afe57af3160a35bcb
syueya <1525817566@qq.com>
2023-10-18 15:11:01 +0800

    导航栏单独文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 const htmlModules = require("./config/htmlModules.js");
-
+const htmlModules = require('./config/nav.js');
 
 module.exports = {
   theme: "vdoing", // 使用依赖包主题
@@ -19,57 +19,7 @@ module.exports = {
 
   // 主题配置
   themeConfig: {
-    nav: [
-      { text: "主页", link: "/" },
-
-      {
-        text: '新手指引',
-        link: '/guide/start/info/',
-        items: [
-          { text: '开始', link: '/guide/start/info/' },
-          { text: '如何安装', link: '/guide/install/start/' },
-          { text: '初始化配置', link: '/guide/init/setting/' },
-          { text: '如何更新', link: '/guide/update_version/start/'},
-        ],
-      },
-
-      {
-        text: '使用文档',
-        link: '/docs/start/info/',
-        items: [
-          { text: '开始', link: '/docs/start/info/' },
-          { text: '功能使用', link: '/docs/use/media_library/' },
-          { text: '系统设置', link: '/docs/setting/base/' },
-          { text: '系统插件', link: '/docs/plugin/info/'},
-          { text: '辅助功能', link: '/docs/tools/info/'},
-          { text: '外联服务', link: '/docs/link_service/info/'},
-          { text: '站点配置', link: '/docs/sites/setting/'},
-          { text: '媒体服务', link: '/docs/setting/ugos/'},
-          { text: '其它', link: '/docs/other/glossary/'},
-        ],
-      },
-
-      { text: "更新日志", link: "/update_version/" },
-
-      {
-        text: '客户端',
-        link: '/client/start/info/',
-        items: [
-          { text: '开始', link: '/guide/start/info/' },
-          { text: 'IOS客户端', link: '/client/ios/info/' },
-          { text: 'Android客户端', link: '/client/android/info/' },
-        ],
-      },
-
-      { text: "赞助", link: "/zhanzhu/" },
-      {
-        text: "反馈",
-        link: "https://github.com/xylplm/media-saber-wiki/issues",
-      },
-      { text: "Docker", link: "https://hub.docker.com/r/xylplm/media-saber" },
-    ],
-
-
+    nav // 插入导航页模块
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: "/img/logo.png", // 导航栏logo
     repo: "xylplm/media-saber-wiki", // 导航栏右侧生成Github链接
