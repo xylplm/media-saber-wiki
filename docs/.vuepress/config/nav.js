a491cf38c3a21286b495fd21aa1ed913cd6ef27b
zjjxwhh <zjjxwhh@163.com>
2024-05-17 12:41:45 +0800

    doc

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -7,8 +7,8 @@ module.exports = [
     items: [
       { text: '开始', link: '/guide/start/info/' },
       { text: '如何安装', link: '/guide/install/start/' },
-      { text: '初始化配置', link: '/guide/init/setting/' },
       { text: '如何更新', link: '/guide/update_version/ugreen/'},
+      { text: '初始化配置', link: '/guide/init/setting/' },
     ],
   },
 
